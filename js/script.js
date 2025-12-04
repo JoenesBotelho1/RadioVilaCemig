490ddcc61d6e6a911144272657aaa82f9dfb39b4
JoenesBotelho1 <104083772+JoenesBotelho1@users.noreply.github.com>
2025-12-04 08:48:43 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 const RADIO_NAME = 'Jailson Web Rádio';
 
 // Change Stream URL Here, Supports, ICECAST, ZENO, SHOUTCAST, RADIOJAR and any other stream service.
-const URL_STREAMING = 'https://stm8.xcast.com.br:10294;stream';
+const URL_STREAMING = 'https://stm8.xcast.com.br:10294/stream';
 
 //API URL /
 const API_URL = 'https://twj.es/free/?url='+URL_STREAMING;
@@ -526,4 +526,5 @@ function intToDecimal(vol) {
 function decimalToInt(vol) {
     return vol * 100;
 } 
+
 
